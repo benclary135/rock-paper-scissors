a671c504628b2c64e87f4ac6d5da40e2a56bb8d2
vagabond <benclary135@gmail.com>
2022-12-15 13:43:13 -0500

    Adjust and implement basic JS functionality in UI.

--- script.js
+++ script.js
@@ -1,60 +1,45 @@
 let result;
-let winCounter = 0;
-let lossCounter = 0;
-//rock==1, paper==2, scissors==3
+let playerScore = 0;
+let computerScore = 0;
+let playerChoiceValue, computerChoiceValue;
+let draw = false;
+const valMap = new Map();
+valMap.set(1, "Rock");
+valMap.set(2, "Paper");
+valMap.set(3, "Scissors");  //rock==1, paper==2, scissors==3
 
 function getComputerChoice() {
     return(Math.ceil(Math.random()*3));
 }
 
-function playRound(playerSelection = prompt("Please choose your weapon!"), computerSelection = getComputerChoice()) {
-    
-    if (playerSelection === null) {
-        alert("Coward!");
+function updateLiveElements() {
+    let playerLiveScore = document.getElementById("player-score");
+    let computerLiveScore = document.getElementById("computer-score");
+    let playerLiveChoice = document.getElementById("you-chose");
+    let computerLiveChoice = document.getElementById("computer-chose");
+
+    playerLiveScore.innerHTML = "Player: " + playerScore;
+    computerLiveScore.innerHTML = "Computer: " + computerScore;
+    playerLiveChoice.innerHTML = valMap.get(playerChoiceValue);
+    computerLiveChoice.innerHTML = valMap.get(computerChoiceValue);
+}
+
+function playRound(playerSelection, computerSelection) {
+
+    const x = playerSelection;
+    const y = computerSelection;
+    playerChoiceValue = x;
+    computerChoiceValue = y;
+
+    if ((x==1&&y==3)||(x==2&&y==1)||(x==3&&y==2)) {  //determine the winner
+        playerScore++;
+    } else if ((x==1&&y==2)||(x==2&&y==3)||(x==3&&y==1)) {
+        computerScore++;
     } else {
-        let playerSelectionLowercase = playerSelection.toLowerCase();
-
-        if (playerSelectionLowercase == "rock") {  //translate player choice to numerical equivalent
-            playerSelection = 1;
-        } else if (playerSelectionLowercase == "paper") {
-            playerSelection = 2;
-        } else if (playerSelectionLowercase == "scissors"){
-            playerSelection = 3;
-        } else {
-            alert("Unknown weapon chosen! Try again.");
-        }
-
-        const x = playerSelection;
-        const y = computerSelection;
-
-        if ((x==1&&y==3)||(x==2&&y==1)||(x==3&&y==2)) {  //determine the winner
-            result = "win";
-            return result;
-        } else if ((x==1&&y==2)||(x==2&&y==3)||(x==3&&y==1)) {
-            result = "loss";
-            return result;
-        } else {
-            result = "draw";
-            return result;
-        }
+        draw = true;
     }
+
+    updateLiveElements();
+    draw = false;
 }
 
-function game() {
-    while (winCounter < 5 && lossCounter < 5) {
-        playRound();
-        console.log(result);
-
-        if (result == "win") {  //increment proper counter
-            winCounter++;
-        } else if (result == "loss") {
-            lossCounter++;
-        }
-
-        if (winCounter == 5) {  //cancel function once a player's score reaches 5
-            console.log("You won!!!");
-        } else if (lossCounter == 5) {
-            console.log("You lost!!!");
-        } 
-    }
-}
